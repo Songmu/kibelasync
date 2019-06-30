9153e30dbfa4fc8a8b805ea74a334a1ec0b05941
Songmu <y.songmu@gmail.com>
2019-06-30 22:59:38 +0900

    TestTime_UnmarshalJSON

--- time_test.go
+++ time_test.go
@@ -20,6 +20,18 @@ type testTime struct {
 	Time Time `json:"time"`
 }
 
+func TestTime_UnmarshalJSON(t *testing.T) {
+	in := `{"time":"2019-06-23T16:54:09.447+09:00"}`
+	var tt testTime
+	if err := json.Unmarshal([]byte(in), &tt); err != nil {
+		t.Errorf("error should be nil but: %s", err)
+	}
+	expect := 9
+	if tt.Time.Second() != expect {
+		t.Errorf("tt.Time.Second() = %d, expect: %d", tt.Time.Second(), expect)
+	}
+}
+
 func TestTime_MarshalJSON(t *testing.T) {
 	in := "2019-06-23T16:54:09.447+09:00"
 	tt := testTime{Time: mustTime(in)}
