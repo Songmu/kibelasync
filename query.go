0d6bff8a5ecc9b51ef41f0d97fed97ce6fef9096
Songmu <y.songmu@gmail.com>
2019-07-01 17:51:21 +0900

    add paginate query

--- query.go
+++ query.go
@@ -41,6 +41,52 @@ func getNoteQuery(id ID) string {
 }`, string(id))
 }
 
+/*
+{
+  "data": {
+    "notes": {
+      "edges": [
+        {
+          "node": {
+            "id": "QmxvZy8zNzA",
+            "updatedAt": "2019-06-23T17:39:47.433+09:00"
+          },
+          "cursor": "NA"
+        },
+        {
+          "node": {
+            "id": "QmxvZy8zNzE",
+            "updatedAt": "2019-06-23T17:40:09.969+09:00"
+          },
+          "cursor": "NQ"
+        },
+        {
+          "node": {
+            "id": "QmxvZy8zNjg",
+            "updatedAt": "2019-06-23T17:39:41.751+09:00"
+          },
+          "cursor": "Ng"
+        }
+      ]
+    }
+  }
+}
+*/
+func listNotePaginateQuery(num int, cursor string) string {
+	// cursor is base64 encoded number. ex. "Nw" = 7
+	return fmt.Sprintf(`{
+  notes(first: %d, after: "%s"){
+    edges {
+      node {
+        id
+        updatedAt
+      }
+      cursor
+    }
+  }
+}`, num, cursor)
+}
+
 const totalGroupCountQuery = `{
   groups() {
     totalCount
