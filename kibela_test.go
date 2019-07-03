14d5e114c10c2a49c7ae209c2422e014f9720507
Songmu <y.songmu@gmail.com>
2019-07-03 23:34:14 +0800

    kibela_test.go

--- kibela_test.go
+++ kibela_test.go
@@ -2,9 +2,11 @@ package kibela
 
 import (
 	"bytes"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 	"reflect"
+	"strings"
 	"testing"
 
 	"github.com/Songmu/kibela/client"
@@ -67,4 +69,49 @@ func TestKibela_setGroups(t *testing.T) {
 	if !reflect.DeepEqual(ki.groups, expect) {
 		t.Errorf("got: %v, expect: %v", ki.groups, expect)
 	}
+}
+
+func TestKibela_fetchGroupID(t *testing.T) {
+	t.Run("fetch error", func(t *testing.T) {
+		ki := testKibela(newClient([]string{`{
+  "errors": [{
+    "message": "error!"
+  }]
+}`}))
+		_, err := ki.fetchGroupID("Home")
+		if err == nil || !strings.Contains(err.Error(), "while setGroupID") {
+			t.Errorf("error should be occurred and contains `while setGroupID`, but: %v", err)
+		}
+	})
+
+	t.Run("fetch success", func(t *testing.T) {
+		expect := "R3JvdXAvMQ"
+		ki := testKibela(newClient([]string{fmt.Sprintf(`{
+  "data": {
+    "groups": {
+      "nodes": [
+        {
+          "id": "%s",
+          "name": "Home"
+        },
+        {
+          "id": "R3JvdXAvMg",
+          "name": "Test"
+        }
+      ]
+    }
+  }
+}`, expect)}))
+		id, err := ki.fetchGroupID("Home")
+		if err != nil {
+			t.Errorf("error should be nil, but: %s", err)
+		}
+		if id != ID(expect) {
+			t.Errorf("got: %s, expect: %s", string(id), expect)
+		}
+		_, err = ki.fetchGroupID("Unknown")
+		if err == nil || !strings.Contains(err.Error(), `group "Unknown"`) {
+			t.Errorf("error should be occurred and contains `group \"Unknown\"`, but: %v", err)
+		}
+	})
 }
